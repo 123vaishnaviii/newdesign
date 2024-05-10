4eaa7413bd53ab1ddddb844a86ad3dc7faca7021
123vaishnaviii <vaishnavivadakkummuri@gmail.com>
2024-05-10 11:58:35 +0530

    test

--- design.js
+++ design.js
@@ -159,6 +159,43 @@
         //     var thumb = jQuery(slider.$slides[i]).data();
         //     return '<a>'+(i+1)+'</a>';
         // } 
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 4,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: true
+              }
+            },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 2
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
         
             });
         })
