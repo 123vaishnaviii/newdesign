a180dd01da749eafcc80c00bccb0434e1503b9f6
123vaishnaviii <vaishnavivadakkummuri@gmail.com>
2024-05-12 01:01:46 +0530

    ADD COLLECTION PAGE

--- design.js
+++ design.js
@@ -109,6 +109,60 @@
         })
 
 
+        $(document).ready(function(){
+          $('.collection-items-row').slick({
+              infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      infinite:true,
+      // autoplay:true,
+      // autoplayspeed:50,
+      dots:true,
+      // customPaging : function(slider, i) {
+      //     var thumb = jQuery(slider.$slides[i]).data();
+      //     return '<a>'+(i+1)+'</a>';
+      // } 
+      responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 4,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+          });
+      })
+
+
 
 
         $(document).ready(function(){
@@ -200,6 +254,12 @@
             });
             $(".menu-icon").click(function () {
               var div = $('.menu-item-view');
+              div.animate({  left: div.css('left') === '0px' ? '-450px' : '0px'
+            }, "");
+            });
+
+            $(".bfinal-part2-btn").click(function () {
+              var div = $('.list-show');
               div.animate({  left: div.css('left') === '0px' ? '-450px' : '0px'
             }, "");
             });
